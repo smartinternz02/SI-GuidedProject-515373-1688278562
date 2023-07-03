65c51aa5a1ca7606eae6c591b581aeb0a3140e3b
Varshika2003 <100751782+Varshika2003@users.noreply.github.com>
2023-07-03 23:08:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+
+import { Routes,Route,BrowserRouter } from 'react-router-dom';
+import Home from './routes/Home';
+import Service from './routes/Service';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import FileUploadForm from './routes/FileUploadForm';
+import DistrictSelectionForm from './routes/DistrictSelectionForm';
+
+import "./App.css"
+import {HashLink as Link} from "react-router-hash-link";
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/FileUploadForm" element={<FileUploadForm/>}/>
+        <Route path="/DistrictSelectionForm" element={<DistrictSelectionForm/>}/>
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
